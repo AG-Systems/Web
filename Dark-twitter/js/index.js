cd1f6a6f115a57ce54d00e583e908f7b739b7953
Auriga <area51alienware@gmail.com>
2016-02-10 23:33:31 -0800

    Added dark twitter template

--- Dark-twitter/js/index.js
+++ Dark-twitter/js/index.js
@@ -1,5 +1,4 @@
 $('.btn_right').click(function(){
   $('.btn_right i').toggleClass('fa-pencil-square-o').toggleClass('fa-times');
    $(".compose-tweet").toggleClass('open');
-});
-$('#newPost').hide();
+}); 
