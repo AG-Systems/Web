d3eb5c20210ef47052a55fe707be353372762d78
Auriga <area51alienware@gmail.com>
2016-02-14 22:35:08 -0800

    Messing around with angulerjs

--- angularjs/js/index.js
+++ angularjs/js/index.js
@@ -1,5 +1,7 @@
-$('.btn_right').click(function(){
+$('.btn_right').click(function()
+{
   $('.btn_right i').toggleClass('fa-pencil-square-o').toggleClass('fa-times');
    $(".compose-tweet").toggleClass('open');
 });
+
 $('#newPost').hide();
