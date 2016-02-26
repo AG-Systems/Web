2fe5e7303c0e4c67123a932c1ce04fb3d89595e7
Auriga <area51alienware@gmail.com>
2016-02-25 21:51:56 -0800

    Working on this random site.

---
+++ angularjs/Practice6/scripts/data/articles.js
@@ -0,0 +1,53 @@
+{
+"articles": [
+      {
+          "title": "Yo I want to buy a evo 8",
+          "description": "Looking for a clean evo. Hit me up fam",
+          "pubDate": "2048-05-01",
+          "tags": [
+                "tag1",
+                "tag4"
+          ]
+      },
+
+
+
+      {
+          "title": "Hello parker",
+          "description": "Feel free to play around.",
+          "pubDate": "2015-03-01",
+          "tags": [
+                "tag2",
+                "tag3",
+                "tag1"
+          ]
+      },
+      {
+          "title": "Omg hai ___^ I’m anon-san and I absolutely luuuv @__@ anime <3 and my fav is naurto!!! ",
+          "description": "ʕ￫ᴥ￩ʔ",
+          "pubDate": "2015-03-01",
+          "tags": [
+                "tag5"
+          ]
+      },
+      {
+          "title": "(づ｡◕‿‿◕｡)づ",
+          "description": "(づ｡◕‿‿◕｡)づ",
+          "pubDate": "2015-03-01",
+          "tags": [
+                "tag5",
+                "tag3",
+                "tag2"
+          ]
+      },
+      {
+          "title": "Good job for clicking view more",
+          "description": "Proud of you (●´ω｀●)",
+          "pubDate": "2015-03-01",
+          "tags": [
+                "tag3",
+                "tag1"
+          ]
+      }
+    ]
+}
