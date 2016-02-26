cb98d23732b729dc11f9a11707ba46117135e0f4
Auriga <area51alienware@gmail.com>
2016-02-25 22:27:23 -0800

    Trying to make it a single page application now.

--- angularjs/Practice6/scripts/data/articles.js
+++ angularjs/Practice6/scripts/data/articles.js
@@ -1,52 +1,23 @@
 {
 "articles": [
       {
-          "title": "Yo I want to buy a evo 8",
-          "description": "Looking for a clean evo. Hit me up fam",
-          "pubDate": "2048-05-01",
+          "title": "Hello everybody",
+          "description": "Still working on the design. Please remeber this will be a long hard project.",
+          "pubDate": "2016-25-02",
           "tags": [
-                "tag1",
-                "tag4"
+                "Market",
+                "Jobs",
+                "Other"
           ]
       },
 
-
-
       {
-          "title": "Hello parker",
-          "description": "Feel free to play around.",
-          "pubDate": "2015-03-01",
+          "title": "More will be coming soon.",
+          "description": "More coming soon (◕ᴥ◕)",
+          "pubDate": "2016-25-02",
           "tags": [
-                "tag2",
-                "tag3",
-                "tag1"
-          ]
-      },
-      {
-          "title": "Omg hai ___^ I’m anon-san and I absolutely luuuv @__@ anime <3 and my fav is naurto!!! ",
-          "description": "ʕ￫ᴥ￩ʔ",
-          "pubDate": "2015-03-01",
-          "tags": [
-                "tag5"
-          ]
-      },
-      {
-          "title": "(づ｡◕‿‿◕｡)づ",
-          "description": "(づ｡◕‿‿◕｡)づ",
-          "pubDate": "2015-03-01",
-          "tags": [
-                "tag5",
-                "tag3",
-                "tag2"
-          ]
-      },
-      {
-          "title": "Good job for clicking view more",
-          "description": "Proud of you (●´ω｀●)",
-          "pubDate": "2015-03-01",
-          "tags": [
-                "tag3",
-                "tag1"
+                "Corps",
+                "Other"
           ]
       }
     ]
