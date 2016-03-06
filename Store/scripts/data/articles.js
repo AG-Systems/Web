09ee4117260422a5f22a0e4a46e1b56c672c0094
Auriga <AG-Systems@users.noreply.github.com>
2016-03-05 20:02:34 -0800

    Started working on a e style site just for fun

---
+++ Store/scripts/data/articles.js
@@ -0,0 +1,24 @@
+{
+"articles": [
+      {
+          "title": "Hello everybody",
+          "description": "Still working on the design. Please remeber this will be a long hard project.",
+          "pubDate": "2016-25-02",
+          "tags": [
+                "Market",
+                "Jobs",
+                "Other"
+          ]
+      },
+
+      {
+          "title": "More will be coming soon.",
+          "description": "More coming soon (◕ᴥ◕)",
+          "pubDate": "2016-25-02",
+          "tags": [
+                "Corps",
+                "Other"
+          ]
+      }
+    ]
+}
---
+++ Store/scripts/modules/listing.js
@@ -0,0 +1,46 @@
+﻿angular.module('listing.module', ['listing.services', 'listing.filters'])
+.controller('listingCtrl', ['$scope', 'data', '$rootScope', function ($scope, data, $rootScope) {
+    'use strict';
+    $scope.title = "Eve Market";
+    $scope.setData = function (data) {
+        $scope.articles = data.articles;
+        console.log($scope.articles);
+    }
+    data.get('scripts/data/articles.js', $scope.setData);
+    $scope.viewLimit = 4;
+    $scope.viewMore = function (num) {
+        $scope.viewLimit += num;
+    }
+    $scope.descending = true;
+    $scope.filters = {
+        query: "",
+        tags: [
+            {
+                label: "Market",
+                selected: false
+            },
+            {
+                label: "Jobs",
+                selected: false
+            },
+            {
+                label: "Corps",
+                selected: false
+            },
+            {
+                label: "PVP",
+                selected: false
+            },
+            {
+                label: "Other",
+                selected: false
+            }
+        ]
+    }
+}])
+.controller('testCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $scope.dataFromRoot = $rootScope.testValue;
+}])
+.run(['$rootScope', function ($rootScope) {
+    $rootScope.testValue = "Practiced by meepo";
+}]);
