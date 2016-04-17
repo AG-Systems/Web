8127c1f6aa7c252d39c1d76ff6c885f29bbfb179
Auriga <AG-Systems@users.noreply.github.com>
2016-04-17 16:15:11 -0700

    Added files via upload

--- NerdStore/nerdscript.js
+++ NerdStore/nerdscript.js
@@ -1,6 +1,26 @@
-( function( $ ) {
-$( document ).ready(function() {
+( function( $ )
+{
+$( document ).ready(function()
+{
+	var counter = 0;
 $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+	$(".container-4").hide();
+	$("#title").hide();
+	$("#title").delay(500).fadeIn(1000);
+	$("#items").hide();
+	$("#items").delay(1000).fadeIn();
+	counter += 1;
+	console.log(counter);
+	if(counter > 0)
+	{
+		$("#items:hover").css({"-o-transition":".5s", "-ms-transition":".5s", "-moz-transition":".5s", "-webkit-transition":".5s","transition":".5s"});
+	}
+	$(".has-sub-search").click(function ()
+	{
+		$(".container-4").fadeIn();
+
+	});
+
 	$('#cssmenu #menu-button').on('click', function(){
 		var menu = $(this).next('ul');
 		if (menu.hasClass('open')) {
